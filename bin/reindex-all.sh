360c2558bf1c3e3664b306d1f0754610133f44fb
dudley.codes <james@lacledeslan.com>
2026-07-19 23:11:18 -0500

    repo structure updates

--- bin/reindex-all.sh
+++ bin/reindex-all.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 REPOS_DIR="${SCRIPT_DIR}/../repos"
 source "${SCRIPT_DIR}/funcs.sh"
 
+mkdir -p "${REPOS_DIR}/gameservers"
 
 #
 # Fetch all repos used by Lacledes LAN dedicated gameservers
--- bin/reindex-tf2classified.sh
+++ bin/reindex-tf2classified.sh
@@ -6,6 +6,8 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 REPOS_DIR="${SCRIPT_DIR}/../repos"
 source "${SCRIPT_DIR}/funcs.sh"
 
+mkdir -p "${REPOS_DIR}/gameservers"
+
 #
 # Fetch all Laclede's LAN repos used by TF2 Classic dedicated servers
 #
